cec4d84ef3aba4c10e329a5c9595bba243c380f6
3310-nokia <919809715@qq.com>
2024-12-19 16:51:10 +0800

    main

---
+++ coloravatar/src/layouts/Footer.vue
@@ -0,0 +1,76 @@
+<template>
+  <footer class="footer">
+    <div
+      data-message="GEAV"
+    >
+      Telegram
+      <a
+        class="link"
+        href="https://t.me/+g1ZuFkA9fJo4OTJl"
+        target="_blank"
+        rel="nofollow noopener noreferrer"
+      >
+        GEAV
+      </a>
+    </div>
+
+    <div class="divider">|</div>
+
+    <div class="locale" @click="switchLocale">
+      {{ locale === Locale.EN ? '简体中文' : 'English' }}
+    </div>
+
+    <div class="divider">|</div>
+
+    <a
+      href="https://zeabur.com?referralCode=Codennnn&utm_source=Codennnn"
+      target="_blank"
+    >
+      <img src="https://zeabur.com/deployed-on-zeabur-dark.svg" />
+    </a>
+  </footer>
+</template>
+
+<script lang="ts" setup>
+import { useI18n } from 'vue-i18n'
+
+import { Locale } from '@/enums'
+
+const { locale } = useI18n()
+
+function switchLocale() {
+  locale.value = locale.value === Locale.EN ? Locale.ZH : Locale.EN
+}
+</script>
+
+<style lang="scss" scoped>
+@use 'src/styles/var';
+
+.footer {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: var.$layout-footer-height;
+  padding: 0.8rem 0;
+  color: rgba(var.$color-text, 0.4);
+  font-weight: bold;
+  font-size: 0.8rem;
+
+  .divider {
+    padding: 0 1rem;
+  }
+
+  .locale {
+    cursor: pointer;
+  }
+
+  .link,
+  .locale {
+    transition: color 0.2s;
+
+    &:hover {
+      color: var.$color-text;
+    }
+  }
+}
+</style>
---
+++ coloravatar/src/layouts/Header.vue
@@ -0,0 +1,91 @@
+<template>
+  <header class="header">
+    <Logo />
+
+    <h2 class="site-title">GEAV</h2>
+
+    <div class="header-right">
+      <a
+        href="https://pump.fun/board"
+        target="_blank"
+        rel="nofollow noopener noreferrer"
+      >
+        <button
+          type="button"
+          class="github-button"
+          @click="
+            recordEvent('click_github', {
+              event_category: 'click',
+            })
+          "
+        >
+          <img :src="IconGitHub" alt="GitHub" />
+          <span class="text">CA:TBA···</span>
+        </button>
+      </a>
+    </div>
+  </header>
+</template>
+
+<script lang="ts" setup>
+import IconGitHub from '@/assets/icons/icon-github.svg'
+import Logo from '@/components/Logo.vue'
+import { recordEvent } from '@/utils/ga'
+</script>
+
+<style lang="scss" scoped>
+@use 'src/styles/var';
+
+.header {
+  display: flex;
+  align-items: center;
+  height: var.$layout-header-height;
+  padding: 1rem 2rem;
+
+  .site-title {
+    margin-left: 1rem;
+    font-weight: bold;
+    font-size: 1.9rem;
+    cursor: default;
+
+    @media screen and (max-width: var.$screen-sm) {
+      display: none;
+    }
+  }
+
+  .header-right {
+    margin-left: auto;
+
+    .github-button {
+      position: relative;
+      z-index: 0;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      width: 12rem;
+      height: 2.6rem;
+      overflow: hidden;
+      color: var.$color-text;
+      font-weight: bold;
+      font-size: 1.05rem;
+      background-color: var.$color-dark;
+      border-radius: 0.6rem;
+      cursor: pointer;
+      user-select: none;
+
+      &:hover {
+        background-color: darken(var.$color-dark, 3);
+      }
+      img {
+      width: 1.5rem;
+      height: 1.5rem; 
+      }
+  
+      .text {
+        margin-left: 0.2rem;
+        letter-spacing: 0.03rem;
+      }
+    }
+  }
+}
+</style>
